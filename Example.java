8c193c77b5e013c7085eba46af325ee5a856e853
Albert Liu <albertymliu@gmail.com>
2020-02-14 21:52:48 -0500

    added initial boi for java

---
+++ Example.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+/*
+ *
+ */
+class Main {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+  }
+}
