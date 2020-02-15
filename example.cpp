c15d8aa67b1365f6a4cd1594e0dbea92f8ab968c
Albert Liu <albertymliu@gmail.com>
2020-02-14 21:54:46 -0500

    added the shiz

---
+++ example.cpp
@@ -0,0 +1,9 @@
+#include <cmath>
+#include <iostream>
+#include <vector>
+
+#define debug(expr)                                                            \
+  std::cerr << "at line " << __LINE__ << ": " << #expr << " equals `" << expr  \
+            << '`' << std::endl;
+
+int main() {}
