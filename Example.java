facb68b2cf59a0bc24b6eae3ee248e64fca71fd8
Albert Liu <albertymliu@gmail.com>
2020-03-25 18:34:38 -0400

    removed disjoint set from example file because usually not necessary

--- Example.java
+++ Example.java
@@ -11,31 +11,6 @@ import java.util.Scanner;
  */
 class Main {
 
-  static int[] disjointSets;
-
-  static int unionFind(int value) {
-    return disjointSets[value] < 0
-        ? value
-        : (disjointSets[value] = unionFind(disjointSets[value]));
-  }
-
-  static int mergeSets(int l, int r) {
-    int lRoot = unionFind(l), rRoot = unionFind(r);
-    if (lRoot == rRoot)
-      return lRoot;
-
-    int lSize = disjointSets[lRoot], rSize = disjointSets[rRoot];
-    int finalSize = lSize + rSize;
-
-    if (lSize < rSize) {
-      disjointSets[lRoot] = finalSize;
-      return disjointSets[rRoot] = lRoot;
-    } else {
-      disjointSets[rRoot] = finalSize;
-      return disjointSets[lRoot] = rRoot;
-    }
-  }
-
   public static void main(String[] args) {
     Scanner input =
         new Scanner(new BufferedReader(new InputStreamReader(System.in)));
