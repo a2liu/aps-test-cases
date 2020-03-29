1a40d8d2defdf7c4da5c6b5ee562923016705565
Albert Liu <albertymliu@gmail.com>
2020-03-29 17:11:14 -0400

    added more to example code

--- Example.java
+++ Example.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
  * Tips:
  * 1. Use TreeSet when you need a min-max heap
  * 2. Use TreeMap when you need a min-max heap with values or an ordered map
- * 3. You can multiply numbers by -1 to turn a Min heap inot a max heap
+ * 3. You can multiply numbers by -1 to turn a Min heap into a max heap
  */
 class Main {
 
--- example.cpp
+++ example.cpp
@@ -8,4 +8,15 @@
   std::cerr << "at line " << __LINE__ << ": " << #expr << " equals `" << expr  \
             << '`' << std::endl;
 
+/*
+ *
+ * Tips:
+ * 1. std::set is ordered, you want std::unordered_set for a hashset
+ * 2. Same for maps, std::unordered_map is the hashmap
+ * 3. You can multiply numbers by -1 to turn a Min heap inot a max heap
+ * 4. #include <algorithm> gets you 90% of the way through most problems;
+ * there's some mighty fine wheels in there, so don't reinvent them if you don't
+ * have to
+ */
+
 int main() {}
