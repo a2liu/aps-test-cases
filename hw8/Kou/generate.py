3240ff3a34f78d109f98ab2031db94ef3a0e6b4c
Albert Liu <albertymliu@gmail.com>
2020-03-29 15:57:57 -0400

    added test cases and generator for hw8/kou

--- hw8/Kou/generate.py
+++ hw8/Kou/generate.py
@@ -2,27 +2,14 @@ import random, os, sys
 
 test_dir = os.path.dirname(os.path.realpath(__file__))
 
-time_step_limit = 12
 
-step_chance = .99
+def generate_test_case(length):
+    arr = [x for x in range(int(length))]
+    random.shuffle(arr)
 
-
-def random_step():
-    return int(time_step_limit * random.random())
-
-
-def step_forwards():
-    return random.random() < step_chance
-
-
-def generate_test_case(n, t, m):
-    lines = [f"{n} {t} {m}"]
-    time = 0
-    for i in range(0, m):
-        if step_forwards():
-            time += random_step()
-        lines.append(f"{time} {random.choice(['left','right'])}")
-    return '\n'.join(lines)
+    final_arr = [str(length)]
+    final_arr.extend(str(x) for x in arr)
+    return "\n".join(final_arr)
 
 
 def main():
@@ -40,8 +27,7 @@ def main():
         if max_case < case_num:
             max_case = case_num
 
-    txt = generate_test_case(int(sys.argv[1]), int(sys.argv[2]),
-                             int(sys.argv[3]))
+    txt = generate_test_case(sys.argv[1])
     file = os.path.join(test_dir, f"test-case-{max_case + 1}")
     with open(file, 'w') as f:
         f.write(txt)
