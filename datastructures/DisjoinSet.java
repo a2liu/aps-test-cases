2fb9efa739afc71b85556411a5ee0e32eecf44e5
Albert Liu <albertymliu@gmail.com>
2020-03-30 12:29:06 -0400

    fixed bug with disjoin set, added example test cases for hw8 party fee

--- datastructures/DisjoinSet.java
+++ datastructures/DisjoinSet.java
@@ -1,28 +1,28 @@
-// Uses size-based ranks. Only optimization not applied is indexing by 1 to avoid
-// set creation requiring two iterations over the array.
+// Uses size-based ranks. Only optimization not applied is indexing by 1 to
+// avoid set creation requiring two iterations over the array.
 public class DisjoinSet {
 
   static int[] newSet(int setCount) {
     int[] array = new int[setCount];
-    for (int i = 0; i < setCount; i ++) {
+    for (int i = 0; i < setCount; i++) {
       array[i] = -1;
     }
     return array;
   }
 
-  static int unionFind(int[] set, int value) {
+  static int find(int[] set, int value) {
     if (set[value] < 0) {
       return value;
     } else {
-      int root = unionFind(set[value]);
+      int root = find(set, set[value]);
       set[value] = root;
       return root;
     }
   }
 
-  static int mergeSets(int[] set, int l, int r) {
+  static int union(int[] set, int l, int r) {
     // Find the root of both of the sets
-    int lRoot = unionFind(set, l), rRoot = unionFind(set, r);
+    int lRoot = find(set, l), rRoot = find(set, r);
 
     if (lRoot == rRoot) // If they're already merged, return
       return lRoot;
@@ -31,11 +31,11 @@ public class DisjoinSet {
     int finalSize = lSize + rSize;
 
     if (lSize < rSize) { // remember, these values are negative
-      disjointSets[lRoot] = finalSize;
+      set[lRoot] = finalSize;
       set[rRoot] = lRoot;
       return lRoot;
     } else {
-      disjointSets[rRoot] = finalSize;
+      set[rRoot] = finalSize;
       set[lRoot] = rRoot;
       return rRoot;
     }
