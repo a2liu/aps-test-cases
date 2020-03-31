937a3beb4d99187ca81dfff3ef31fe140eaadf23
Albert Liu <albertymliu@gmail.com>
2020-03-30 23:08:54 -0400

    added moar test cases

--- restructure-files.py
+++ restructure-files.py
@@ -6,7 +6,6 @@ folder_path = os.path.realpath(sys.argv[1])
 for name in os.listdir(folder_path):
     path = os.path.join(folder_path, name)
     if os.path.isdir(path) and name.isdigit():
-        print(name)
         input_path = os.path.join(path, 'input')
         output_path = os.path.join(path, 'output')
         os.rename(input_path, os.path.join(folder_path, 'test-case-' + name))
