6eac55be24f6a91ad46266aee3745cc7acb7439a
Albert Liu <albertymliu@gmail.com>
2020-03-30 23:05:08 -0400

    added test cases for find sums

---
+++ restructure-files.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import os, sys
+
+folder_path = os.path.realpath(sys.argv[1])
+
+for name in os.listdir(folder_path):
+    path = os.path.join(folder_path, name)
+    if os.path.isdir(path) and name.isdigit():
+        print(name)
+        input_path = os.path.join(path, 'input')
+        output_path = os.path.join(path, 'output')
+        os.rename(input_path, os.path.join(folder_path, 'test-case-' + name))
+        os.rename(output_path,
+                  os.path.join(folder_path, 'test-case-' + name + '-ans'))
