f83b7edc580809268d8e8c684b113f1687e795f3
Albert Liu <albertymliu@gmail.com>
2020-04-16 19:05:56 -0400

    made example.cpp better, added another bus test case

--- example.cpp
+++ example.cpp
@@ -1,22 +1,29 @@
 #include <algorithm>
 #include <cmath>
+#include <iomanip>
 #include <iostream>
 #include <sstream>
 #include <vector>
 
+using namespace std;
+
 #define debug(expr)                                                            \
-  std::cerr << "at line " << __LINE__ << ": " << #expr << " equals `" << expr  \
-            << '`' << std::endl;
+  cerr << "\033[1;31m"                                                         \
+       << "at line " << __LINE__ << ": "                                       \
+       << "\033[0m" << #expr << " equals `" << expr << '`' << std::endl;
 
 /*
- *
  * Tips:
  * 1. std::set is ordered, you want std::unordered_set for a hashset
  * 2. Same for maps, std::unordered_map is the hashmap
- * 3. You can multiply numbers by -1 to turn a Min heap inot a max heap
+ * 3. You can multiply numbers by -1 to turn a Min heap into a max heap
  * 4. #include <algorithm> gets you 90% of the way through most problems;
  * there's some mighty fine wheels in there, so don't reinvent them if you don't
  * have to
  */
 
-int main() {}
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+}
